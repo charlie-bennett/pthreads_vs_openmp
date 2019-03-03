dde7c9ca2b325e2dcbc5140153a38d20fbc7d74b
Charles Bennett <cfbennet@usc.edu>
2019-03-02 16:19:25 -0800

    fixed

--- p1_serial.c
+++ p1_serial.c
@@ -12,39 +12,43 @@ typedef struct
 
 int main(void)
 {
+
 	const int NUM_THREADS_ARRAY[] = {1, 2, 4};
-	int i;
+	int i, index;
 	int num_of_points_in_circle;
 	double pi;
 	struct timespec start, stop;
 	double time;
 	Point* data_point = (Point*) malloc (sizeof(Point) * num_of_points);
-	for (i = 0; i < num_of_points; i++)
+	for (index = 0; index < 3; index++)
 	{
-		data_point[i].x = (double)rand() / (double)RAND_MAX;
-		data_point[i].y = (double)rand() / (double)RAND_MAX;
-	}
-	num_of_points_in_circle = 0;
-
-	omp_set_num_threads(NUM_THREADS_ARRAY[index]);
-
-	if ( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
-
-
-	////////**********Use OpenMP to parallize this loop***************//
-	#pragma omp parallel for reduction(+:num_of_points_in_circle)
-	for (i = 0; i < num_of_points; i++)
-	{
-		if ((data_point[i].x - 0.5) * (data_point[i].x - 0.5) + (data_point[i].y - 0.5) * (data_point[i].y - 0.5) <= 0.25)
+		for (i = 0; i < num_of_points; i++)
 		{
-			num_of_points_in_circle++;
+			data_point[i].x = (double)rand() / (double)RAND_MAX;
+			data_point[i].y = (double)rand() / (double)RAND_MAX;
 		}
+		num_of_points_in_circle = 0;
+
+		omp_set_num_threads(NUM_THREADS_ARRAY[index]);
+
+		if ( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+
+
+		////////**********Use OpenMP to parallize this loop***************//
+		#pragma omp parallel for reduction(+:num_of_points_in_circle)
+		for (i = 0; i < num_of_points; i++)
+		{
+			if ((data_point[i].x - 0.5) * (data_point[i].x - 0.5) + (data_point[i].y - 0.5) * (data_point[i].y - 0.5) <= 0.25)
+			{
+				num_of_points_in_circle++;
+			}
+		}
+		///////******************************////
+
+		if ( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}
+		time = (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / 1e9;
+
+		pi = 4 * (double)num_of_points_in_circle / (double)no_of_points;
+		printf("Estimated pi is %f, execution time = %f sec\n",  pi, time);
 	}
-	///////******************************////
-
-	if ( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}
-	time = (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / 1e9;
-
-	pi = 4 * (double)num_of_points_in_circle / (double)no_of_points;
-	printf("Estimated pi is %f, execution time = %f sec\n",  pi, time);
 }
